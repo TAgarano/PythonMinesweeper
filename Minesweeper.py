c134fbc7983f468875cc3fbcff1ec7e480658964
TAgarano <60578983+TAgarano@users.noreply.github.com>
2020-04-18 15:41:30 -0500

    Update Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -7,11 +7,89 @@
 # Created by: Travis A, Andrew H, Matthew P
 #
 
+#
+# imports
+#
+import random as r
 from tkinter import *
-from Minesweeper_mines import *
 
+#
+# globals
+#
 # list of buttons
 button_locations = []
+
+# list for the map
+field = [[0 for i in range(11)] for j in range(11)]
+
+# mine count
+num_mines = 18
+
+
+# used to place the mines
+def mines():
+    global field
+
+    i = 0
+    while i != num_mines:
+        x = r.randint(0, 10)
+        y = r.randint(0, 10)
+
+        # place mine
+        if field[x][y] != 9:
+            field[x][y] = 9
+            i += 1
+    for i in range(len(field)):
+        print(field[i])
+    print()
+
+
+# increment numbers around mines
+def increment_neighbors():
+    global field
+
+    for row in range(len(field)):
+        for col in range(len(field[row])):
+            # mine
+            if field[row][col] == 9:
+
+                # To the right
+                if col + 1 < len(field) and field[row][col + 1] != 9:
+                    field[row][col + 1] += 1
+
+                # Below the mine
+                if row + 1 < len(field) and field[row + 1][col] != 9:
+                    field[row + 1][col] += 1
+
+                # Above the mine
+                if row - 1 >= 0 and field[row - 1][col] != 9:
+                    field[row - 1][col] += 1
+
+                # To the left
+                if col - 1 >= 0 and field[row][col - 1] != 9:
+                    field[row][col - 1] += 1
+
+                # Down right
+                if row + 1 < len(field) and col + 1 < len(field[row]) and field[row + 1][col + 1] != 9:
+                    field[row + 1][col + 1] += 1
+
+                # Up right
+                if row - 1 >= 0 and col + 1 < len(field[row]) and field[row - 1][col + 1] != 9:
+                    field[row - 1][col + 1] += 1
+
+                # Up left
+                if row - 1 >= 0 and col - 1 >= 0 and field[row - 1][col - 1] != 9:
+                    field[row - 1][col - 1] += 1
+
+                # Down left
+                if row + 1 < len(field) and col - 1 >= 0 and field[row + 1][col - 1] != 9:
+                    field[row + 1][col - 1] += 1
+
+
+# adds mines and the numbers to list: fields
+def create_map():
+    mines()
+    increment_neighbors()
 
 
 # everything that goes into the window
@@ -74,8 +152,48 @@ def button_grid(window):
 
 
 def clicked(index):
+    button_locations[int(index)]["state"] = DISABLED
     button_locations[int(index)].place_forget()
 
+    # Button above
+    if (int(index) - 1) % 11 <= 9 and int(index) - 1 < 121 and field[int(index) % 11][int(int(index) / 11)] == 0\
+            and button_locations[int(index) - 1]["state"] == NORMAL:
+        clicked(int(index) - 1)
+
+    # Button left
+    if int(index) - 11 > 0 and field[int(index) % 11][int(int(index) / 11)] == 0\
+            and button_locations[int(index) - 11]["state"] == NORMAL:
+        clicked(int(index) - 11)
+
+    # Button down
+    if (int(index) + 1) % 11 != 0 and field[int(index) % 11][int(int(index) / 11)] == 0\
+            and button_locations[int(index) + 1]["state"] == NORMAL:
+        clicked(int(index) + 1)
+
+    # Button right
+    if int(index) + 11 < 121 and field[int(index) % 11][int(int(index) / 11)] == 0\
+            and button_locations[int(index) + 11]["state"] == NORMAL:
+        clicked(int(index) + 11)
+
+    # Button right/down
+    if int(index) + 12 < 121 and (int(index) + 12) % 11 != 0 and field[int(index) % 11][int(int(index) / 11)] == 0\
+            and button_locations[int(index) + 12]["state"] == NORMAL:
+        clicked(int(index) + 12)
+
+    # Button right/up
+    if (int(index) + 10) % 11 != 10 and int(index) + 10 < 121 and field[int(index) % 11][int(int(index) / 11)] == 0 \
+            and button_locations[int(index) + 10]["state"] == NORMAL:
+        clicked(int(index) + 10)
+
+    # Button left/up
+    if int(index) - 12 > 0 and (int(index) + 10) % 11 != 10 and field[int(index) % 11][int(int(index) / 11)] == 0\
+            and button_locations[int(index) - 12]["state"] == NORMAL:
+        clicked(int(index) - 12)
+
+    # Button left/down
+    if int(index) - 10 > 0 and (int(index) + 12) % 11 != 0 and field[int(index) % 11][int(int(index) / 11)] == 0\
+            and button_locations[int(index) - 10]["state"] == NORMAL:
+        clicked(int(index) - 10)
 
 def main():
     # creates window
